a7a32e032683523157a8ce01f802452156488f37
LudovicCocquyt <ludlud59@hotmail.fr>
2020-06-29 15:45:58 +0200

    maj fixture

--- EH/src/DataFixtures/AppFixtures.php
+++ EH/src/DataFixtures/AppFixtures.php
@@ -5,7 +5,9 @@ namespace App\DataFixtures;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Realisations;
 use App\Entity\StaticContent;
+use App\Entity\Images;
 use App\Entity\User;
 
 class AppFixtures extends Fixture
@@ -19,6 +21,7 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
+        //fixture User Admin
 		$user = new User;
 		$now  = new \Datetime;
 
@@ -26,18 +29,25 @@ class AppFixtures extends Fixture
              ->setPassword($this->encoder->encodePassword($user, 'aa'))
         	 ->setemail('aa@aa.fr')
              ->setIsActif(true)
-             ->setRoles(['ROLE_USER']);
+             ->setRoles(['ROLE_ADMIN']);
 
         $manager->persist($user);
 
-		$contentOne   = new StaticContent;
-		$contentTwo   = new StaticContent;
-		$contentThree = new StaticContent;
+
+        //fixture content static
+        $contentOne   = new StaticContent;
+        $contentTwo   = new StaticContent;
+        $contentThree = new StaticContent;
+        $contentFour  = new StaticContent;
+        $contentFive  = new StaticContent;
+        $contentSix   = new StaticContent;
+        $contentSeven = new StaticContent;
 
         $contentOne->setCreatedAt($now)
         ->setCreatedBy('admin')
         ->setUpdatedAt($now)
         ->setUpdatedBy('admin')
+        ->setTitle('Présentation 1')
         ->setContent("Auto-entrepreneur du bâtiment situé à Trith Saint Léger (59), je vous propose mes services pour vos travaux.");
         $manager->persist($contentOne);
 
@@ -45,6 +55,7 @@ class AppFixtures extends Fixture
         ->setCreatedBy('admin')
         ->setUpdatedAt($now)
         ->setUpdatedBy('admin')
+        ->setTitle('TVA')
         ->setContent("En me confiant vos travaux vous bénéficiez d'une TVA non applicable.	");
         $manager->persist($contentTwo);
 
@@ -52,8 +63,54 @@ class AppFixtures extends Fixture
         ->setCreatedBy('admin')
         ->setUpdatedAt($now)
         ->setUpdatedBy('admin')
+        ->setTitle('Présentation 2')
         ->setContent("Terrassement - Maçonnerie - Couverture - Carrelage - Tout à l'égout - Menuiserie - Peinture - Plâtrerie	");
         $manager->persist($contentThree);
+
+        $contentFour->setCreatedAt($now)
+        ->setCreatedBy('admin')
+        ->setUpdatedAt($now)
+        ->setUpdatedBy('admin')
+        ->setTitle('Adresse')
+        ->setContent("23 rue Gustave Delory 59125 Trith Saint Leger");
+        $manager->persist($contentFour);
+
+        $contentFive->setCreatedAt($now)
+        ->setCreatedBy('admin')
+        ->setUpdatedAt($now)
+        ->setUpdatedBy('admin')
+        ->setTitle('Téléphone')
+        ->setContent("06 50 42 27 37");
+        $manager->persist($contentFive);
+
+        $contentSix->setCreatedAt($now)
+        ->setCreatedBy('admin')
+        ->setUpdatedAt($now)
+        ->setUpdatedBy('admin')
+        ->setTitle('Email')
+        ->setContent("cousinHub@gmail.com");
+        $manager->persist($contentSix);
+
+        $contentSeven->setCreatedAt($now)
+        ->setCreatedBy('admin')
+        ->setUpdatedAt($now)
+        ->setUpdatedBy('admin')
+        ->setTitle('Lien iframe de google')
+        ->setContent('https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2546.6910821867273!2d3.5016645157272066!3d50.33501437945947!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c2ed00ef2c147d%3A0x3efbec32d3410730!2s23%20Rue%20Gustave%20Delory%2C%2059125%20Trith-Saint-L%C3%A9ger!5e0!3m2!1sfr!2sfr!4v1592496177330!5m2!1sfr!2sfr');
+        $manager->persist($contentSeven);
+
+        //fixture realisation
+        $realisation = new Realisations;
+
+        $realisation->setTitle('Ma réalisation')
+                    ->setDescription('La description de ma réalisation')
+                    ->setSection('Sol');
+        $manager->persist($realisation);
+
+        $image = new Images;
+        $image->setRealisations($realisation)
+              ->setName('not_delete.png');
+        $manager->persist($image);
 
         $manager->flush();
     }
