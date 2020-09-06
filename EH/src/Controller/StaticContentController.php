2a7da45f501cf942362eee4c2b88663578d3d7d4
LudovicCocquyt <ludlud59@hotmail.fr>
2020-09-06 16:26:58 +0200

    desactiver contact

--- EH/src/Controller/StaticContentController.php
+++ EH/src/Controller/StaticContentController.php
@@ -49,34 +49,34 @@ class StaticContentController extends AbstractController
         ]);
     }
 
-    // /**
-    //  * @Route("/new", name="static_content_new", methods={"GET","POST"})
-    //  */
-    // public function new(Request $request): Response
-    // {
-    //     $staticContent = new StaticContent();
-    //     $form = $this->createForm(StaticContentType::class, $staticContent);
-    //     $form->handleRequest($request);
+    /**
+     * @Route("/new", name="static_content_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $staticContent = new StaticContent();
+        $form = $this->createForm(StaticContentType::class, $staticContent);
+        $form->handleRequest($request);
 
-    //     if ($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
 
-    //         $staticContent->setCreatedAt(new \DateTime('now'));
-    //         $staticContent->setCreatedBy('ludo');
-    //         $staticContent->setUpdatedAt(new \DateTime('now'));
-    //         $staticContent->setUpdatedBy('ludo');
+            $staticContent->setCreatedAt(new \DateTime('now'));
+            $staticContent->setCreatedBy('ludo');
+            $staticContent->setUpdatedAt(new \DateTime('now'));
+            $staticContent->setUpdatedBy('ludo');
 
-    //         $entityManager = $this->getDoctrine()->getManager();
-    //         $entityManager->persist($staticContent);
-    //         $entityManager->flush();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($staticContent);
+            $entityManager->flush();
 
-    //         return $this->redirectToRoute('static_content_index');
-    //     }
+            return $this->redirectToRoute('static_content_index');
+        }
 
-    //     return $this->render('static_content/new.html.twig', [
-    //         'static_content' => $staticContent,
-    //         'form' => $form->createView(),
-    //     ]);
-    // }
+        return $this->render('static_content/new.html.twig', [
+            'static_content' => $staticContent,
+            'form' => $form->createView(),
+        ]);
+    }
 
     /**
      * @Route("/{id}", name="static_content_show", methods={"GET"})
